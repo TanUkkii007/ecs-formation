877a3212765bab94594a6b1bed44de1d855c98fe
stormcat24 <a.yamada24@gmail.com>
2015-07-28 01:00:20 +0900

    :art: add global option 'sns-topic'

--- operation/commands.go
+++ operation/commands.go
@@ -49,7 +49,7 @@ var commandBluegreen = cli.Command{
 	Flags: []cli.Flag{
 		cli.BoolFlag{
 			Name: "nodeploy, nd",
-			Usage: "bbb",
+			Usage: "without deployment, only swap load balancer",
 		},
 	},
 	Action: doBluegreen,
